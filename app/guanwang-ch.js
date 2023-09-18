c285cf3b2e35aeb9a081f0b706c28ae7247cadda
Mr-Nobody-li <lpf13015886562@gmail.com>
2023-09-18 11:24:12 +0800

    feat: init

---
+++ app/guanwang-ch.js
@@ -0,0 +1,97 @@
+const platformInfo = {
+  // 公司信息
+  companyInfo: {
+    name: "浙江君同智能科技有限责任公司",
+    address: "浙江省杭州市西湖区文三路",
+    email: "xxx",
+    phone: "xxx",
+    // 合作伙伴列表
+    Partners: [
+      {
+        // 合作伙伴logo
+        coverImage: "https://xxx.jpg",
+      },
+      {
+        coverImage: "https://xxx.jpg",
+      },
+    ],
+  },
+  // 产品列表
+  products: [
+    {
+      // 名称
+      name: "君侧",
+      // 简称
+      info: "智能模型防火墙",
+      // 首页产品介绍的描述
+      HomePageDesc: "xxx",
+      // 首页产品介绍的标签
+      HomePageTags: ["xxx", "xxx"],
+      // 首页产品介绍的背景图
+      backgroundImage: "https://xxx.jpg",
+      // 列表页的描述
+      desc: "xxx",
+      // 列表页的产品封面图
+      coverImage: "https://xxx.jpg",
+      // 产品详情页的框架图
+      frameworkImage: "https://xxx.jpg",
+      // 产品详情页的产品功能
+      abilities: [
+        {
+          title: "xxx",
+          desc: "防范数据模型内生安全以及外部攻击提供持续测试服务",
+          coverUrl: "https://xxx.jpg",
+        },
+      ],
+      // 产品详情页的产品优势
+      advantages: [
+        {
+          title: "xxx",
+          desc: "防范数据模型内生安全以及外部攻击提供持续测试服务",
+        },
+      ],
+      // 产品详情页的应用场景
+      ApplicationScenarios: [
+        {
+          // 场景名称
+          sceneName: "评测机构",
+          // 场景英文名称
+          sceneEngName: "Rating Agency",
+          // 场景描述
+          desc: "xxx",
+        },
+      ],
+    },
+  ],
+  // 服务支持
+  serviceSupports: [
+    {
+      // 服务名称
+      name: "服务介绍",
+      // 服务描述
+      desc: "xxx",
+      // 服务封面图
+      coverImage: "https://xxx.jpg",
+      // 首页服务图标
+      iconName: "",
+    },
+  ],
+  // 解决方案
+  solutions: [
+    {
+      // 解决方案名称
+      name: "企业",
+      // 解决方案描述
+      desc: "xxx",
+      // 解决方案封面图
+      coverImage: "https://xxx.jpg",
+    },
+    {
+      name: "政府",
+      desc: "xxx",
+      coverImage: "https://xxx.jpg",
+    },
+  ],
+};
+
+export default platformInfo;
