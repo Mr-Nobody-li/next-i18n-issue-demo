204f6c1e5bd6732724ad6e34019de7badeb9e175
Mr-Nobody-li <lpf13015886562@gmail.com>
2023-09-19 10:28:12 +0800

    feat: antd 引入

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,30 @@
-import type { Config } from 'tailwindcss'
+/*
+ * @Author: Mr-Nobody-li
+ * @Date: 2023-09
+ * @LastEditors: Mr-Nobody-li
+ * @LastEditTime: 2023-09
+ * @Description: tailwind
+ */
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
-export default config
+  corePlugins: {
+    preflight: false,
+  },
+};
+export default config;
---
+++ theme/themeConfig.ts
@@ -0,0 +1,18 @@
+/*
+ * @Author: Mr-Nobody-li
+ * @Date: 2023-09
+ * @LastEditors: Mr-Nobody-li
+ * @LastEditTime: 2023-09
+ * @Description:
+ */
+import type { ThemeConfig } from "antd";
+
+const theme: ThemeConfig = {
+  token: {
+    fontSize: 14,
+    colorPrimary: "#2576c3",
+    fontFamily: "inherit",
+  },
+};
+
+export default theme;
