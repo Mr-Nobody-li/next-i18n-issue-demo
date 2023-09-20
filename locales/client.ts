73b00c3130fb5b6e2563d412d5cb8e6f4a7b4b2b
Mr-Nobody-li <lpf13015886562@gmail.com>
2023-09-20 09:00:05 +0800

    feat: i18n插件配置

--- locales/client.ts
+++ locales/client.ts
@@ -8,6 +8,6 @@
 import { createI18nClient } from "next-international/client";
 
 export const { useI18n, useScopedI18n, I18nProviderClient } = createI18nClient({
-  en: () => import("./en"),
-  zh: () => import("./zh"),
+  en: () => import("./en.json"),
+  zh: () => import("./zh-CN.json"),
 });
--- locales/server.ts
+++ locales/server.ts
@@ -8,6 +8,6 @@
 import { createI18nServer } from "next-international/server";
 
 export const { getI18n, getScopedI18n, getStaticParams } = createI18nServer({
-  en: () => import("./en"),
-  zh: () => import("./zh"),
+  en: () => import("./en.json"),
+  zh: () => import("./zh-CN.json"),
 });
